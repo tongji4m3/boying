18bf761cf6d91f034d8fdf6615e55f3b2f2a2ac4
tongji4m3 <46219861+tongji4m3@users.noreply.github.com>
2020-09-01 10:36:48 +0800

    Initial commit

---
+++ src/main/java/com/tongji/MySpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.tongji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+    public MySpringBootApplication() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, new String[0]);
+    }
+}
