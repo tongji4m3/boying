af673f4b50afb5c0893669c522ad5b015a2444eb
tongji4m3 <1254931237@qq.com>
2020-10-11 10:55:46 +0800

    整合Swagger

--- src/main/java/com/tongji/controller/UserController.java
+++ src/main/java/com/tongji/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.tongji.controller;
 
 import com.tongji.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
+@Api(tags = "UserController", description = "用户信息管理")
 public class UserController
 {
     @Autowired
@@ -16,6 +21,7 @@ public class UserController
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation("获取用户信息")
     public String test()
     {
         userService.select(1);
