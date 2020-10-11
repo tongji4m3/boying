1967f1faac6a78b65f278c2ae5f4857d0527dd09
tongji4m3 <1254931237@qq.com>
2020-10-11 11:11:41 +0800

    整合Redis环境

--- src/main/java/com/tongji/service/impl/UserServiceImpl.java
+++ src/main/java/com/tongji/service/impl/UserServiceImpl.java
@@ -2,17 +2,20 @@ package com.tongji.service.impl;
 
 import com.tongji.mbg.mapper.UserMapper;
 import com.tongji.mbg.model.User;
+import com.tongji.service.RedisService;
 import com.tongji.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService
 {
-    @Autowired
+    @Autowired(required = false)
     private UserMapper userMapper;
+
+    @Autowired
+    private RedisService redisService;
 
     @Override
     public User select(int id)
@@ -24,6 +27,16 @@ public class UserServiceImpl implements UserService
         user.setPassword("AXXXX");
         System.out.println(userMapper.insert(user));
         System.out.println(user);
+        if(redisService.get("user")==null)
+        {
+            System.out.println("Redis First time ----");
+            redisService.set("user",user.toString());
+        }
+        else
+        {
+            System.out.println("Redis next time ----");
+            System.out.println(redisService.get("user"));
+        }
         return user;
     }
 }
