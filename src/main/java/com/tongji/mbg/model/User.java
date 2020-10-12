3f4585801ac3885195b77c55a1b78993cd0d61c5
tongji4m3 <1254931237@qq.com>
2020-10-12 22:26:10 +0800

    修改generator的版本

--- src/main/java/com/tongji/mbg/model/User.java
+++ src/main/java/com/tongji/mbg/model/User.java
@@ -5,9 +5,9 @@ import java.io.Serializable;
 public class User implements Serializable {
     private Integer id;
 
-    private String username;
-
     private String password;
+
+    private String username;
 
     private static final long serialVersionUID = 1L;
 
@@ -19,20 +19,20 @@ public class User implements Serializable {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     @Override
@@ -42,8 +42,8 @@ public class User implements Serializable {
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
-        sb.append(", username=").append(username);
         sb.append(", password=").append(password);
+        sb.append(", username=").append(username);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
