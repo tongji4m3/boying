75bf32cec42d3ee55fb57b2cc7739b199fc5e859
Cffuu <1289079621@qq.com>
2021-01-10 23:08:04 +0800

    ·

--- softEngineer/boying/boying-admin/src/main/java/com/tongji/boying/controller/NumsTicketController.java
+++ softEngineer/boying/boying-admin/src/main/java/com/tongji/boying/controller/NumsTicketController.java
@@ -25,7 +25,7 @@ public class NumsTicketController {
     private NumsTicketService numsTicketService;
 
     @ApiOperation("查看票信息")
-    @RequestMapping(value = "/listTickets", method = RequestMethod.GET)
+    @RequestMapping(value = "/listTickets", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult<CommonPage<BoyingTicket>> listTickets(@Validated @RequestBody TicketParam param) {
         return CommonResult.success(CommonPage.restPage(numsTicketService.listTickets(param)));
