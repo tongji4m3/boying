b20b5503424997ca5156cc3686715eb0bc121e9b
tongji4m3 <1254931237@qq.com>
2021-01-22 19:06:44 +0800

    更新

--- softEngineer/boying/boying-user/src/main/java/com/tongji/boying/service/ShowSeatService.java
+++
@@ -1,9 +0,0 @@
-package com.tongji.boying.service;
-
-import com.tongji.boying.model.BoyingSeat;
-
-import java.util.List;
-
-public interface ShowSeatService {
-    List<BoyingSeat> getShowSeatList(Integer showId);
-}
