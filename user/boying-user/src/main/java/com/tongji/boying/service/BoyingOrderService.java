ea41bc47baeb411366aed4f8e649741e8133db1a
tongji4m3 <1254931237@qq.com>
2021-02-17 06:45:04 +0800

    笔记

--- user/boying-user/src/main/java/com/tongji/boying/service/BoyingOrderService.java
+++ user/boying-user/src/main/java/com/tongji/boying/service/BoyingOrderService.java
@@ -3,6 +3,7 @@ package com.tongji.boying.service;
 import com.tongji.boying.dto.orderParam.GetOrdersParam;
 import com.tongji.boying.dto.orderParam.UserOrderParam;
 import com.tongji.boying.model.BoyingOrder;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ public interface BoyingOrderService {
      * 添加订单
      * 通过前端url上传过来秒杀活动id，然后下单接口内校验对应id是否属于对应商品且活动已开始
      */
+    @Transactional
     void add(UserOrderParam param);
 
     /**
@@ -18,9 +20,11 @@ public interface BoyingOrderService {
      *
      * @param id 订单表的id
      */
+    @Transactional
     void delete(int id);
 
 
+    @Transactional
     void cancel(int id);
 
     void finish(int id);
