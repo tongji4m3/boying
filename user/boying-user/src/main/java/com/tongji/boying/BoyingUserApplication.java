99b88d1ec5eed154f314ae6d4a3ddfb57759cfb6
tongji4m3 <1254931237@qq.com>
2021-05-08 14:27:05 +0800

    删除.idea文件

--- user/boying-user/src/main/java/com/tongji/boying/BoyingUserApplication.java
+++ user/boying-user/src/main/java/com/tongji/boying/BoyingUserApplication.java
@@ -3,6 +3,8 @@ package com.tongji.boying;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 @SpringBootApplication(scanBasePackages = "com.tongji.boying")
 public class BoyingUserApplication {
     public static void main(String[] args) {
