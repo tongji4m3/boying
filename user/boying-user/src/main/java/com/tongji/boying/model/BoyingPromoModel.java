9f37f812daa72efd060521f6522b0d8efce148b7
tongji4m3 <1254931237@qq.com>
2021-05-19 13:52:02 +0800

    文档

--- user/boying-user/src/main/java/com/tongji/boying/model/BoyingPromoModel.java
+++ user/boying-user/src/main/java/com/tongji/boying/model/BoyingPromoModel.java
@@ -14,7 +14,7 @@ import java.util.Date;
 public class BoyingPromoModel {
     private Integer id;
 
-    //秒杀活动状态 1表示还未开始，2表示进行中，3表示已结束
+    //秒杀活动状态 0表示没有秒杀活动 1表示还未开始，2表示进行中，3表示已结束
     private Integer status;
 
     @ApiModelProperty(value = "促销活动对应的座次")
