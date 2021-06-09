0dd24d2ca7d7bc15d456c372fe32e11384c3248b
Cffuu <1289079621@qq.com>
2021-06-09 20:32:52 +0800

    check活动时间

--- admin/boying-admin/src/main/java/com/tongji/boying/service/impl/SmsPromoSeviceImpl.java
+++ admin/boying-admin/src/main/java/com/tongji/boying/service/impl/SmsPromoSeviceImpl.java
@@ -4,8 +4,12 @@ import cn.hutool.core.collection.CollUtil;
 import com.tongji.boying.common.exception.Asserts;
 import com.tongji.boying.dto.UmsPromoParam;
 import com.tongji.boying.mapper.BoyingPromoMapper;
+import com.tongji.boying.mapper.BoyingSeatMapper;
+import com.tongji.boying.mapper.BoyingShowMapper;
 import com.tongji.boying.model.BoyingPromo;
 import com.tongji.boying.model.BoyingPromoExample;
+import com.tongji.boying.model.BoyingSeat;
+import com.tongji.boying.model.BoyingShow;
 import com.tongji.boying.service.SmsPromoService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,27 +22,43 @@ import java.util.List;
  * 后台活动管理Service实现类
  */
 @Service
-public class SmsPromoSeviceImpl implements SmsPromoService {
+public class SmsPromoSeviceImpl implements SmsPromoService
+{
     @Autowired
     private BoyingPromoMapper boyingPromoMapper;
+    @Autowired
+    private BoyingSeatMapper boyingSeatMapper;
+    @Autowired
+    private BoyingShowMapper boyingShowMapper;
 
     @Override
-    public List<BoyingPromo> list() {
+    public List<BoyingPromo> list()
+    {
         return boyingPromoMapper.selectByExample(new BoyingPromoExample());
     }
 
     @Override
-    public BoyingPromo getPromo(Integer id) {
+    public BoyingPromo getPromo(Integer id)
+    {
         return boyingPromoMapper.selectByPrimaryKey(id);
     }
 
     @Override
-    public int create(UmsPromoParam param) {
+    public int create(UmsPromoParam param)
+    {
         BoyingPromoExample boyingPromoExample = new BoyingPromoExample();
         boyingPromoExample.createCriteria().andSeatIdEqualTo(param.getSeatId());
         List<BoyingPromo> boyingPromos = boyingPromoMapper.selectByExample(boyingPromoExample);
-        for (BoyingPromo boyingPromo : boyingPromos) {
-            if (boyingPromo.getEndTime().after(new Date())) {
+        BoyingSeat seat = boyingSeatMapper.selectByPrimaryKey(param.getSeatId());
+        BoyingShow show = boyingShowMapper.selectByPrimaryKey(seat.getShowId());
+        if (!(param.getEndTime().before(show.getEndTime()) && param.getStartTime().after(show.getStartTime())))
+        {
+            Asserts.fail("该活动时间未位于演出时间范围内!");
+        }
+        for (BoyingPromo boyingPromo : boyingPromos)
+        {
+            if (boyingPromo.getEndTime().after(new Date()))
+            {
                 Asserts.fail("该座次有未结束活动!");
             }
         }
@@ -48,7 +68,8 @@ public class SmsPromoSeviceImpl implements SmsPromoService {
         return boyingPromoMapper.insertSelective(promo);
     }
 
-    public int update(Integer id, UmsPromoParam param) {
+    public int update(Integer id, UmsPromoParam param)
+    {
         BoyingPromo promo = new BoyingPromo();
 
         BeanUtils.copyProperties(param, promo);
@@ -57,29 +78,35 @@ public class SmsPromoSeviceImpl implements SmsPromoService {
     }
 
     @Override
-    public int delete(Integer id) {
+    public int delete(Integer id)
+    {
         return boyingPromoMapper.deleteByPrimaryKey(id);
     }
 
     @Override
-    public int delete(List<Integer> ids) {
+    public int delete(List<Integer> ids)
+    {
         BoyingPromoExample example = new BoyingPromoExample();
         example.createCriteria().andIdIn(ids);
-        if (boyingPromoMapper.selectByExample(example).size() != ids.size()) {
+        if (boyingPromoMapper.selectByExample(example).size() != ids.size())
+        {
             Asserts.fail("某些演出Id不存在！");
         }
         return boyingPromoMapper.deleteByExample(example);
     }
 
-    private void checkBoyingPromoParam(UmsPromoParam param, Integer id) {
+    private void checkBoyingPromoParam(UmsPromoParam param, Integer id)
+    {
         BoyingPromoExample boyingPromoExample = new BoyingPromoExample();
         BoyingPromoExample.Criteria criteria = boyingPromoExample.createCriteria();
         criteria.andNameEqualTo(param.getName());
-        if (id != -1) {
+        if (id != -1)
+        {
             criteria.andIdNotEqualTo(id);
         }
         List<BoyingPromo> promos = boyingPromoMapper.selectByExample(boyingPromoExample);
-        if (CollUtil.isNotEmpty(promos)) {
+        if (CollUtil.isNotEmpty(promos))
+        {
             Asserts.fail("活动名称不能重复");
         }
     }
